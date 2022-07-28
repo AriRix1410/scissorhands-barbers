d57c3d85ea71ba9988df743b31ffffdcdc897704
AriRix1410 <aa.fraserhaste@gmail.com>
2022-07-28 20:06:27 +0000

    Install django-summernote

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer, Barber, Booking
 
 # Register your models here.
+
+
+admin.site.register(Customer)
+admin.site.register(Barber)
+admin.site.register(Booking)
