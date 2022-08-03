6ec8f541ea3b0a68b2e3c7cf8cf3382b521784ae
AriRix1410 <aa.fraserhaste@gmail.com>
2022-08-03 19:49:36 +0000

    Add views and forms for booking requests

--- booking/forms.py
+++ booking/forms.py
@@ -1,8 +1,16 @@
 from .models import Booking
+import datetime
 from django import forms
+
+
+class DatePicker(forms.DateInput):
+    input_type = 'date'
 
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('date', 'time', 'barber', 'service',)
+        widgets = {
+            'date': DatePicker(),
+        }
