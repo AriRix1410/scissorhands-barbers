a820957611e6a773ef579215030d0a79f31a3db2
AriRix1410 <aa.fraserhaste@gmail.com>
2022-08-27 00:12:36 +0000

    Update image paths

--- scissorhands/settings.py
+++ scissorhands/settings.py
@@ -175,7 +175,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/static/assets/'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
